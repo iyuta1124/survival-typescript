9f8f8ca3ae8b86683c431ef391d2aee444938f99
inoue-yu <iyuta1124@gmail.com>
2023-07-22 20:59:57 +0900

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,77 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import { NextPage } from "next";
+import { useEffect, useState } from "react";
+import styles from "./index.module.css";
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
-  </Layout>
-)
+type Props = {
+  initialImageUrl: string;
+};
 
-export default IndexPage
+const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
+  const [imageUrl, setImageUrl] = useState(initialImageUrl);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    fetchImage().then((newImage) => {
+      setImageUrl(newImage.url);
+      setLoading(false);
+    });
+  }, []);
+  // ボタンをクリックしたときに画像を読み込む処理
+  const handleClick = async () => {
+    setLoading(true); // 読込中フラグを立てる
+    const newImage = await fetchImage();
+    setImageUrl(newImage.url); // 画像URLの状態を更新する
+    setLoading(false); // 読込中フラグを倒す
+  };
+  return (
+    <div className={styles.page}>
+      <button onClick={handleClick} className={styles.button}>
+        他のにゃんこも見る
+      </button>
+      <div className={styles.frame}>
+        {loading || <img src={imageUrl} className={styles.img} />}
+      </div>
+    </div>
+  );
+};
+
+export default IndexPage;
+
+// export const GetServerSideProps: GetServerSideProps<Props> = async () => {
+//   const image = await fetchImage();
+//   return {
+//     props: {
+//       initialImageUrl: image.url,
+//     },
+//   };
+// };
+
+type Image = {
+  url: string;
+};
+
+const fetchImage = async (): Promise<Image> => {
+  const res = await fetch("https://api.thecatapi.com/v1/images/search");
+  const images: unknown = await res.json();
+  if (!Array.isArray(images)) {
+    throw new Error("猫画像が取得できません");
+  }
+  const image: unknown = images[0];
+  // Imageの構造をなしているか？
+  if (!isImage(image)) {
+    throw new Error("猫の画像が取得できませんでした");
+  }
+  return image;
+};
+
+fetchImage().then((image) => {
+  console.log(image);
+});
+
+const isImage = (value: unknown): value is Image => {
+  // 値がオブジェクトなのか？
+  if (!value || typeof value !== "object") {
+    return false;
+  }
+  // urlプロパティが存在し、かつ、それが文字列なのか？
+  return "url" in value && typeof value.url === "string";
+};
